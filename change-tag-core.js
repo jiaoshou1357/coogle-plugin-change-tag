926ec4e3d3862df68ffa6b5f7130759a85c422fe
飞 <871513958@qq.com>
2024-09-13 18:40:10 +0800

    Update change-tag-core.js

--- change-tag-core.js
+++ change-tag-core.js
@@ -1,8 +1,10 @@
-var currency=null
-var keywords = null;
-var isReplaced = false;
 
-//防抖
+var firstReplaced = false;
+var oldHtml = document.body.innerHTML;
+var scrollTop = 0; 
+const regex = /[@#$]\w+/g; 
+
+//简易防抖函数
 function disShake(fun,delay) {
   var timer ;
   return function () {
@@ -12,45 +14,66 @@ function disShake(fun,delay) {
       },delay)
   }
 }
-function replaceWord() {
-  if(isReplaced) return;
-  var regex_2 = new RegExp('\\b('+currency+')[\\w]+\\b','g');
-  //var htmlText = document.body.innerHTML.replace(/<\/?[\w\s="/.':;#-\/\?]+>/gi,  '');
-  var matches = document.body.innerHTML.match(regex_2);
-  var _matches = [...new Set(matches)]
-  if (_matches) {
-    for (let j = 0; j < _matches.length; j++) {
-      let span = document.createElement("span");
-      span.style.backgroundColor = "yellow";
-      //添加类，为事件委托做准备
-      span.className = "yellow";
-      span.textContent = _matches[j];
-      if(keywords) {
-          span.textContent=keywords+'\x20'+span.textContent
+//获取url参数
+function getUrlParams(url) {
+	let urlStr = url.split('?')[1]
+	const urlSearchParams = new URLSearchParams(urlStr)
+	const result = Object.fromEntries(urlSearchParams.entries())
+	return result
+}
+function replaceTextInDocument(node) {
+  const ulrParam = getUrlParams(window.location.href)
+  const query = ulrParam.q||''
+  if (node.nodeType === Node.TEXT_NODE) {
+    var matches =  node.textContent.match(regex);
+    var _matches = [...new Set(matches)]
+      for (let j = 0; j < _matches.length; j++) {
+          if(node.textContent.indexOf(query+"\x20"+_matches[j])<=-1) {
+            let newNode = document.createElement("span");
+            newNode.style.backgroundColor = "yellow";
+            //添加类，为事件委托做准备
+            newNode.className = "yellow";
+            newNode.textContent = query+"\x20"+_matches[j];
+            node.parentNode&&node.parentNode.replaceChild(newNode,node)
+         } 
       }
-      document.body.innerHTML = document.body.innerHTML.replace(new RegExp(_matches[j], "g"), span.outerHTML);
+  } else if (node.nodeType === Node.ELEMENT_NODE) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      replaceTextInDocument(node.childNodes[i]);
     }
   }
-  isReplaced = true;
 }
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.currency && request.currency.length > 0) {
-       currency =  request.currency;
-       keywords =  request.keywords;
-       maxScrollTop = 0;
-       isReplaced = false;
-       if(request.isAction) {
-        replaceWord()
-       }
+//替换操作
+function replaceInnerHtml() {
+  replaceTextInDocument(document.body);
+  oldHtml = document.body.innerHTML;
+  firstReplaced = true;
+}
+
+function scrollAction() {
+  //利用事件循环队列机制检查是否有新的加载的html
+  setTimeout(function(){
+    scrollTop = document.documentElement.scrollTop || document.body.scrollTop;;
+    if(document.body.innerHTML!==oldHtml) {
+      //拿到新的html部分并且替换
+      replaceInnerHtml()
+      window.scrollTo(0, scrollTop);
+      oldHtml = document.body.innerHTML;
     }
-});
+  },1)
+  if(firstReplaced) return;
+  replaceInnerHtml() 
+}
+
 document.addEventListener('click',function(e){
   if(e.target.className=='yellow') {
     alert("我被点击了！")
   }
 },true)
-var move = disShake(replaceWord,300)
+var move = disShake(scrollAction,300)
 document.addEventListener('scroll',move,true)
+
+
 
 
 
